f603518b928c18f956380fb2e86ae67861babc62
Patrik Dufresne <ikus060@gmail.com>
2013-01-04 13:09:15 -0500

    Modification de ManagerException afin d'offrir une fonction pour déterminé sur l'erreur provient d'une violation de contrainte (ContraintViolationException).

--- managers/src/com/patrikdufresne/managers/ManagerException.java
+++ managers/src/com/patrikdufresne/managers/ManagerException.java
@@ -1,5 +1,7 @@
 package com.patrikdufresne.managers;
 
+import org.hibernate.exception.ConstraintViolationException;
+
 /**
  * This exception is throw when any type of error occurred within the execution
  * of a Manager.
@@ -13,19 +15,73 @@ public class ManagerException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	/**
+	 * Create a new exception without cause.
+	 */
 	public ManagerException() {
 
 	}
 
+	/**
+	 * Constructs a new exception with the specified detail message. The cause
+	 * is not initialized, and may subsequently be initialized by a call to
+	 * {@link #initCause}.
+	 * 
+	 * @param message
+	 *            the detail message. The detail message is saved for later
+	 *            retrieval by the {@link #getMessage()} method.
+	 */
 	public ManagerException(String message) {
 		super(message);
 	}
 
+	/**
+	 * Constructs a new exception with the specified detail message and cause.
+	 * <p>
+	 * Note that the detail message associated with <code>cause</code> is
+	 * <i>not</i> automatically incorporated in this exception's detail message.
+	 * 
+	 * @param message
+	 *            the detail message (which is saved for later retrieval by the
+	 *            {@link #getMessage()} method).
+	 * @param cause
+	 *            the cause (which is saved for later retrieval by the
+	 *            {@link #getCause()} method). (A <tt>null</tt> value is
+	 *            permitted, and indicates that the cause is nonexistent or
+	 *            unknown.)
+	 */
 	public ManagerException(String message, Throwable cause) {
 		super(message, cause);
 	}
 
+	/**
+	 * Constructs a new exception with the specified cause and a detail message
+	 * of <tt>(cause==null ? null : cause.toString())</tt> (which typically
+	 * contains the class and detail message of <tt>cause</tt>). This
+	 * constructor is useful for exceptions that are little more than wrappers
+	 * for other throwables (for example,
+	 * {@link java.security.PrivilegedActionException}).
+	 * 
+	 * @param cause
+	 *            the cause (which is saved for later retrieval by the
+	 *            {@link #getCause()} method). (A <tt>null</tt> value is
+	 *            permitted, and indicates that the cause is nonexistent or
+	 *            unknown.)
+	 * @param cause
+	 */
 	public ManagerException(Throwable cause) {
 		super(cause);
+	}
+
+	/**
+	 * Check if the exception is a constraint violation exception.
+	 * <p>
+	 * It's recommended to use this function instead of checking the type of the
+	 * cause.
+	 * 
+	 * @return True if this exception is a contraint violation exception.
+	 */
+	public boolean isConstraintViolationException() {
+		return getCause() instanceof ConstraintViolationException;
 	}
 }
