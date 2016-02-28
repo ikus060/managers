3d389581b0dc0613298c15e368b514c2cce85ec2
Patrik Dufresne <ikus060@gmail.com>
2016-02-28 07:59:30 -0500

    Add option to open a copy.

--- com.patrikdufresne.managers/src/main/java/com/patrikdufresne/managers/H2DBConfigurations.java
+++ com.patrikdufresne.managers/src/main/java/com/patrikdufresne/managers/H2DBConfigurations.java
@@ -1,21 +1,20 @@
 /**
  * Copyright(C) 2013 Patrik Dufresne Service Logiciel <info@patrikdufresne.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
  */
 package com.patrikdufresne.managers;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.Field;
@@ -23,6 +22,7 @@ import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.NetworkInterface;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.LinkedList;
@@ -39,19 +39,32 @@ import org.hibernate.dialect.H2Dialect;
 public class H2DBConfigurations {
 
     /**
-     * Create a new configuration to prepare a Managers.
+     * Used to copy a file.
      * 
-     * @param dbUrl
-     *            the database url. Either a file url or a jdbc url
-     * @param create
-     *            True to create the file
-     * @param autoServer
-     *            True to automatically create a server.
-     * @return the configuration for the managers.
-     * @throws MalformedURLException
+     * @param sourceFile
+     * @param destFile
+     * @throws IOException
      */
-    public static Configuration create(String dbUrl, boolean create, boolean autoServer) throws MalformedURLException {
-        return create(dbUrl, create, autoServer, false);
+    private static void copyFile(File sourceFile, File destFile) throws IOException {
+        if (!destFile.exists()) {
+            destFile.createNewFile();
+        }
+
+        FileChannel source = null;
+        FileChannel destination = null;
+
+        try {
+            source = new FileInputStream(sourceFile).getChannel();
+            destination = new FileOutputStream(destFile).getChannel();
+            destination.transferFrom(source, 0, source.size());
+        } finally {
+            if (source != null) {
+                source.close();
+            }
+            if (destination != null) {
+                destination.close();
+            }
+        }
     }
 
     /**
@@ -66,7 +79,33 @@ public class H2DBConfigurations {
      * @return the configuration for the managers.
      * @throws MalformedURLException
      */
-    public static Configuration create(String dbUrl, boolean create, boolean autoServer, boolean readonly) throws MalformedURLException {
+    public static Configuration create(String dbUrl, boolean create, boolean autoServer) throws MalformedURLException {
+        try {
+            return create(dbUrl, create, autoServer, false, false);
+        } catch (MalformedURLException e) {
+            throw e;
+        } catch (IOException e) {
+            throw new MalformedURLException(e.getMessage());
+        }
+    }
+
+    /**
+     * Create a new configuration to prepare a Managers.
+     * 
+     * @param dbUrl
+     *            the database url. Either a file url or a jdbc url
+     * @param create
+     *            True to create the file
+     * @param autoServer
+     *            True to automatically create a server.
+     * @param readonly
+     *            Open the database in read only mode.
+     * @param copy
+     *            Open a copy of the database.
+     * @return the configuration for the managers.
+     * @throws IOException
+     */
+    public static Configuration create(String dbUrl, boolean create, boolean autoServer, boolean readonly, boolean copy) throws IOException {
         if (dbUrl == null) {
             throw new IllegalArgumentException();
         }
@@ -94,6 +133,15 @@ public class H2DBConfigurations {
         // Enable Hibernate's automatic session context management
         config.setProperty(Environment.CURRENT_SESSION_CONTEXT_CLASS, ThreadLocalSessionContext.class.getCanonicalName());//$NON-NLS-1$ 
 
+        // Copy the database
+        if (copy && url.isLocal()) {
+            File src = new File(url.getAbsolutePath() + ".h2.db");
+            File dest = File.createTempFile("planimod-temp", ".h2.db");
+            copyFile(src, dest);
+            dest.deleteOnExit();
+            url = new H2DBDatabaseUrl(dest.getAbsolutePath());
+        }
+
         /*
          * Rebuild the url.
          */
@@ -116,7 +164,7 @@ public class H2DBConfigurations {
         } else {
             config.setProperty(Environment.HBM2DDL_AUTO, "dummy");
         }
-        if(readonly){
+        if (readonly) {
             buf.append(";ACCESS_MODE_DATA=r");
         }
         config.setProperty(Environment.URL, buf.toString());
